c415623c164eb9e214a4d15e836fedd093a3068a
Roderick <rjrr507@gmail.com>
2022-04-13 20:41:43 -0500

    Ajustes de Migracion

--- migrations/102_CreateServidores.js
+++ migrations/102_CreateServidores.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "Usuarios",
+          model: "UsuariosServidores",
           key: "idUsuario",
         },
       },
